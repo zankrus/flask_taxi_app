2e66b5429b1e63b703b119c31ce40d73659eeb75
r.islyamov <sealthepirate@gmail.com>
2020-07-07 15:10:18 +0300

    add delete for Driver

--- database.py
+++ database.py
@@ -37,9 +37,9 @@ class Drivers(Base):
             session.commit()
     def show_drivers(self, id):
         with session_scope() as session:
-            player_by_class = session.query(Drivers).filter(Drivers.id == id).all()
-            print(str(player_by_class))
-            return player_by_class
+            show_dr = session.query(Drivers).filter(Drivers.id == id).all()
+            print(str(show_dr))
+            return show_dr
     def delete_driver(self, id):
         with session_scope() as session:
             session.query(Drivers).filter(Drivers.id == id).delete()
@@ -58,6 +58,19 @@ class Clients(Base):
     name = Column(String, nullable=False)
     is_vip = Column(Boolean)
 
+    def insert_clients(self, name, vip: bool= False):
+        with session_scope() as session:
+            session.add(Clients(name=name, is_vip=vip))
+            session.commit()
+    def show_clients(self, id):
+        with session_scope() as session:
+            show_clie = session.query(Clients).filter(Clients.id == id).all()
+            print(str(show_clie))
+            return show_clie
+    def delete_clients(self, id):
+        with session_scope() as session:
+            session.query(Clients).filter(Clients.id == id).delete()
+            session.commit()
 
 class Orders(Base):
     Session = sessionmaker(bind=engine)
--- simple_app.py
+++ simple_app.py
@@ -1,15 +1,21 @@
 from flask import Flask
 from flask import request, Response
 from flask import json
-from database import Drivers
+from database import Drivers, Clients
 
 app = Flask(__name__)
 
 
 @app.route('/drivers/<int:post_id>')
-def shod_driver_profile(post_id):
-    new_driver = Drivers()
-    return str(new_driver.show_drivers(post_id))
+def show_driver_profile(post_id):
+    try:
+        new_driver = Drivers()
+        resp = str(new_driver.show_drivers(post_id))
+        if resp == '[]':
+            return Response('Объект не найден в базе', status=404)
+        return resp
+    except Exception:
+        return Response('Неправильный запрос', status=400)
 
 
 @app.route('/drivers', methods=['POST', 'DELETE'])
@@ -22,24 +28,47 @@ def driver():
     if request.method == 'POST':
         try:
             new_driver.insert_drivers(json_from_request['name'], json_from_request['car'])
+            print('Post' + str(json_from_request))
+            return Response('Created', status=201)
+        except Exception:
+            return Response('Неправильный запрос', status=400)
+    elif request.method == 'DELETE':
+        try:
+            print('Delete')
+            print(json_from_request)
+            if str(new_driver.show_drivers(json_from_request['id'])) == '[]':
+                return Response('Объект не найден в базе', status=404)
+            new_driver.delete_driver(json_from_request['id'])
+            return Response('Удалено', status=204)
+        except Exception:
+            return Response('Неправильный запрос', status=400)
+
+
+@app.route('/clients/<int:client_id>')
+def show_client_profile(client_id):
+    new_client = Clients()
+    return str(new_client.show_clients(client_id))
+
+
+@app.route('/clients', methods=['POST', 'DELETE'])
+def client():
+    try:
+        new_client = Clients()
+        json_from_request = json.loads(request.data.decode('utf-8'))
+    except Exception:
+        return Response('Произошла ошибка', status=400)
+
+    if request.method == 'POST':
+        try:
+            new_client.insert_clients(json_from_request['name'], json_from_request['is_vip'])
             return Response('Created', status=201)
         except Exception:
             return Response('Произошла ошибка', status=400)
     elif request.method == 'DELETE':
         print(json_from_request)
         print((json_from_request['id']))
-        new_driver.delete_driver(json_from_request['id'])
+        new_client.delete_clients(json_from_request['id'])
         return Response('Deleted', status=201)
-
-
-@app.route('/clients', methods=['GET', 'POST', 'DELETE'])
-def client():
-    if request.method == 'GET':
-        return 'Sasai'
-    elif request.method == 'POST':
-        pass
-    elif request.method == 'DELETE':
-        pass
 
 
 @app.route('/orders', methods=['GET', 'POST', 'DELETE'])
